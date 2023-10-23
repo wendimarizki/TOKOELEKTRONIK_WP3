fd73448c481863db8393a6320776f21dd40752b3
wendimarizki <wendimarizkidinata@gmail.com>
2023-10-23 10:06:23 +0700

    upload awal project

---
+++ application/views/templates_customer/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer-->
+<footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="<?php echo base_url('assets/assets_shop') ?>/https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo base_url('assets/assets_shop') ?>/js/scripts.js"></script>
+    </body>
+</html>
